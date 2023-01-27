7f4b478c566abb4003a700f0a43ea917e27bd117
Oleg Vysotsky <olegbom@gmail.com>
2023-01-27 15:55:37 +0300

    shell pwsh

--- init.lua
+++ init.lua
@@ -1,5 +1,13 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+-- vim.o.shell = 'powershell'
+vim.o.shell = 'pwsh'
+vim.o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+vim.o.shellquote = ' '
+vin.o.shellxquote = ' '
+
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
