aca752fc1ae49335998189508ce79e8321dee9d3
Oleg Vysotsky <olegbom@gmail.com>
2023-01-27 16:40:48 +0300

    Fix pwsh console

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ vim.o.shell = 'pwsh'
 vim.o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
 vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-vim.o.shellquote = ' '
-vin.o.shellxquote = ' '
+vim.o.shellquote = ''
+vim.o.shellxquote = ''
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
